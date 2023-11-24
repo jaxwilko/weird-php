f1c0a3a5f00b89568a554a8435416555c2cf3dc0
Jack Wilkinson <me@jackwilky.com>
2023-11-24 08:48:43 +0000

    Added send method to Hint

--- src/Messages/Events/Hint.php
+++ src/Messages/Events/Hint.php
@@ -7,5 +7,12 @@ use Weird\Messages\GenericMessage;
 
 class Hint extends GenericMessage implements Message
 {
+    public static function send(mixed $message): void
+    {
+        if (!defined('WEIRD_SPAWNED_PROCESS')) {
+            return;
+        }
 
+        processWrite(new static($message));
+    }
 }
