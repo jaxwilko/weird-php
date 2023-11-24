20174fa50076ba78986b41f9530cf7d6a16b0d28
Jack Wilkinson <me@jackwilky.com>
2023-11-24 08:53:07 +0000

    Added test cases

--- tests/ProcessManagerTest.php
+++ tests/ProcessManagerTest.php
@@ -9,6 +9,12 @@ class ProcessManagerTest extends TestCase
 {
     protected string $bootstrapFile = __DIR__ . '/bootstrap.php';
 
+    /**
+     * Validate that the ProcessManager can spawn processes
+     *
+     * @return void
+     * @throws \Weird\Exceptions\ProcessSpawnFailed
+     */
     public function testSpawnProcess(): void
     {
         $manager = ProcessManager::create()
@@ -32,6 +38,13 @@ class ProcessManagerTest extends TestCase
         $this->assertEquals(0, $manager->processesCount());
     }
 
+    /**
+     * Validate that Promise can be executed and next functions executed on return
+     *
+     * @return void
+     * @throws \Weird\Exceptions\ProcessFailed
+     * @throws \Weird\Exceptions\ProcessSpawnFailed
+     */
     public function testExecutePromise(): void
     {
         $manager = ProcessManager::create()
@@ -66,6 +79,13 @@ class ProcessManagerTest extends TestCase
         $this->assertEquals('working', $result);
     }
 
+    /**
+     * Validate that non-promise executables can be executed
+     *
+     * @return void
+     * @throws \Weird\Exceptions\ProcessFailed
+     * @throws \Weird\Exceptions\ProcessSpawnFailed
+     */
     public function testExecuteCallable(): void
     {
         $manager = ProcessManager::create()
@@ -83,5 +103,67 @@ class ProcessManagerTest extends TestCase
         $this->assertEquals('hello', file_get_contents(__DIR__ . '/test.txt'));
 
         unlink(__DIR__ . '/test.txt');
+    }
+
+    /**
+     * Validate that registered hint handlers are executed when a hint is returned by a process
+     *
+     * @return void
+     * @throws \Weird\Exceptions\ProcessFailed
+     * @throws \Weird\Exceptions\ProcessSpawnFailed
+     */
+    public function testHinting(): void
+    {
+        $manager = ProcessManager::create()
+            ->withBootstrap($this->bootstrapFile)
+            ->spawn(Thread::class);
+
+        $manager->registerHintHandler(function (mixed $message) {
+            $this->assertEquals('test', $message);
+        });
+
+        // Test with scoping
+        $manager->dispatch(
+            Promise::make(function () {
+                \Weird\Messages\Events\Hint::send('test');
+                return 'working';
+            })
+                ->then(function (string $output) {
+                    $this->assertEquals('working', $output);
+                })
+        );
+
+        $manager->wait();
+    }
+
+    /**
+     * Validate that registered unknown message handlers are executed when a hint is returned by a process
+     *
+     * @return void
+     * @throws \Weird\Exceptions\ProcessFailed
+     * @throws \Weird\Exceptions\ProcessSpawnFailed
+     */
+    public function testUnknownMessage(): void
+    {
+        $manager = ProcessManager::create()
+            ->withBootstrap($this->bootstrapFile)
+            ->spawn(Thread::class);
+
+        $manager->registerUnknownMessageHandler(function (mixed $message) {
+            $this->assertEquals('test', $message);
+        });
+
+        // Test with scoping
+        $manager->dispatch(
+            Promise::make(function () {
+                echo 'test';
+                return 'working';
+            })
+                ->then(function (string $output) {
+                    $this->assertEquals('working', $output);
+                })
+        );
+
+        $manager->wait();
     }
 }
